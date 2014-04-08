efca6acbe4ea389d2d0b1e686f827557e702186e
Peter <peterhsaxton@gmail.com>
2014-04-08 16:30:08 +0100

    Added Failing test

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -12,4 +12,10 @@ describe AppController do
     expect(last_response).to be_ok
     expect(last_response.body).to include('Fizzbuzz')
   end
+
+  it 'should respond with 1 when visiting root/1' do
+    get '/1'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('1')
+  end
 end
