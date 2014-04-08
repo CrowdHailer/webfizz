ada0dc2aaeeb1cc0e0447c5f2560f4c8159fefce
Peter <peterhsaxton@gmail.com>
2014-04-08 16:22:35 +0100

    Created homepage response

--- app.rb
+++ app.rb
@@ -1,4 +1,7 @@
 require 'sinatra'
 
 class AppController < Sinatra::Base
+  get '/' do
+    'Fizzbuzz'
+  end
 end
--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -6,4 +6,10 @@ describe AppController do
   def app
     AppController
   end
+
+  it 'should have a homepage response' do
+    get '/'
+    expect(last_response).to be_ok
+    expect(last_response.body).to include('Fizzbuzz')
+  end
 end
